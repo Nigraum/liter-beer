b1ed9bbd74deb0cd51b6f84c1166299c4dae2efa
Nigraum <yan.corintima@hotmail.com>
2022-10-18 18:35:17 -0300

    Setting app.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,51 @@
-import { ColorSchemeProvider, MantineProvider } from "@mantine/core";
-import React from "react";
-import ButtonTheme from "../components/ButtonTheme";
-import RowsTable from "../components/RowsTable";
-import Table from "../components/Table";
+import { Box, ColorScheme, ColorSchemeProvider, Container, MantineProvider, Title } from "@mantine/core";
+import { useState } from "react";
+import { DataProps } from "../@types";
 
 function App() {
-  return(
-    <Table/>
-  );
-}; 
+  const [pack1, setPack1] = useState<string>("unit");
+  const [arrayTable, setArrayTable] = useState<DataProps[]>([]);
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+
+
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme((prev) => value || (prev === "dark" ? "light" : "dark"));
+
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme }}
+      >
+        <Box
+          p="20px"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <Container
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              width: "320px"
+            }}
+          >
+            <Title style={{ textAlign: "center", color: "#352F29"}}>
+              Preço/Litro
+            </Title>
+          </Container>
+        </Box>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  )
+}
 
 export default App;
