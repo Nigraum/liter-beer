f9ad2396d2fca440b81a380003a59d05c365f4d8
Nigraum <yan.corintima@hotmail.com>
2022-10-18 17:37:53 -0300

    Finishing rowstable index.tsx

--- src/components/RowsTable/index.tsx
+++ src/components/RowsTable/index.tsx
@@ -1,6 +1,53 @@
-const RowsTable = () => {
+import { ActionIcon } from "@mantine/core";
+import { IconTrashX } from "@tabler/icons";
+import {TableProps} from "../../@types/index";
+
+interface RowsTableProps {
+  tableData: TableProps;
+  onDeleteRow: (index: number) => void;
+  index: number;
+}
+
+const RowsTable = ({ tableData, onDeleteRow, index }: RowsTableProps) => {
+  console.log(tableData, "receive table data")
   return (
-    <h1>LLKKKKKKK</h1>
+    <tr>
+      <td>{tableData.name}</td>
+      <td>
+        {tableData.mls}
+        {" "}mls
+      </td>
+      <td>{tableData.amount}</td>
+      <td>
+        {new Intl.NumberFormat("pt-Br", {
+          currency: "BRL",
+          style: "currency",
+        }).format(tableData?.price)}
+      </td>
+      <td>
+        {tableData.desc}
+        {" "}%
+      </td>
+      <td>
+        {new Intl.NumberFormat("pt-BR", {
+          currency: "BRL",
+          style: "currency",
+        }).format(tableData?.total)}
+      </td>
+      <td>
+        <ActionIcon
+          color="yellow"
+          variant="transparent"
+          id={index.toString()}
+          onClick={(e: any) => {
+            console.log(e.target.closest("button").id);
+            onDeleteRow(Number(e.target.closest("button").id));
+          }}
+        >
+          <IconTrashX size={26} />
+        </ActionIcon>
+      </td>
+    </tr>
   );
 };
 
