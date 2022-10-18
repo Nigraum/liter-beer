c45db8107e469f092962b80b7098d1cbeaaa643d
Nigraum <yan.corintima@hotmail.com>
2022-10-18 16:16:28 -0300

    Create theme.jsx

---
+++ src/Theme/Theme.jsx
@@ -0,0 +1,29 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  fonts: {
+    especial: "ArchitectsDaughter, sans-serif",
+    body: "Roboto, sans-serif",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "black.200",
+        fontFamily: "Roboto, sans-serif",
+        color: "white.100",
+      },
+      input: {
+        fontWeight: "700",
+        borderRadius: "7px",
+        boxShadow: "0px 13px 50px rgba(0, 0, 0, 0.1)",
+      },
+      option: {
+        fontSize: "18px",
+        fontWeight: "600",
+      },
+    },
+  },
+  headings: {
+    fontFamily: "Roboto, sans-serif", 
+  },
+});
