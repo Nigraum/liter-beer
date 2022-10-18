843cf3af2be75fa334f27f27e533aa741a08fb1f
Nigraum <yan.corintima@hotmail.com>
2022-10-18 18:09:48 -0300

    Almost done table index, need to create a calc func to rowstable work

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,6 +1,42 @@
+import { Table as TableMantine } from "@mantine/core";
+import React, { useEffect, useMemo, useState } from "react";
+import { DataProps, TableProps } from "../../@types";
+import { IconDiscount } from "@tabler/icons";
+import RowsTable from "../RowsTable";
+
+interface LogicProps {
+  data: DataProps[];
+  setArrayTable: React.Dispatch<React.SetStateAction<DataProps[]>>;
+};
+
 const Table = () => {
   return (
-    <h1>Testandooooo</h1>
+    <TableMantine striped highlightOnHover style={{ maxWidth: "320px" }}>
+      <thead
+        style={{
+          background: "#D9D9D9",
+          color: "#352F29",
+        }}
+      >
+        <tr>
+          <th>Cerveja</th>
+          <th>Mls</th>
+          <th>Qtd</th>
+          <th>R$</th>
+          <th>
+            <IconDiscount />
+          </th>
+          <th>R$/L</th>
+          <th>{" "}</th>
+        </tr>
+      </thead>
+      <tbody
+        style={{
+          color: "#352F29",
+        }}
+      >
+      </tbody>
+    </TableMantine>
   );
 };
 
